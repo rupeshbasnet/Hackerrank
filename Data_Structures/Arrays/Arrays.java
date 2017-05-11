ccaac321cfb99d4be20c9609926e0bb5a8d76195
rupeshbasnet <rupeshbasnet93@gmail.com>
2017-05-11 01:22:06 -0400

    added Arrays.java

---
+++ Data_Structures/Arrays/Arrays.java
@@ -0,0 +1,27 @@
+/*
+Given an array,A ,of N integers, print each element in reverse order as a single line of space-separated integers.
+
+Print all N integers in A in reverse order as a single line of space-separated integers.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        int temp;
+        for(int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
