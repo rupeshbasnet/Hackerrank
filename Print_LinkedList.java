ea07672ab8a7703bddff77b2d2f07b26f4755cd1
rupeshbasnet <rupeshbasnet93@gmail.com>
2017-06-09 17:34:24 -0400

    Add files via upload

---
+++ Print_LinkedList.java
@@ -0,0 +1,24 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+    
+void Print(Node head) {
+  Node temp = head;
+  if (temp == null) {
+      return;
+  } else {
+      while (temp != null) {
+          System.out.println(temp.data);
+          temp = temp.next;
+      }
+  }
+}
